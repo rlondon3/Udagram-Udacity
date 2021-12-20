df976dd8c8c5a8d7d9198ec9a10ff5a97da713c5
Raphael Smith <codesmithr@outlook.com>
2021-12-19 21:17:37 -0800

    env fix

--- udagram/udagram-api/src/config/config.ts
+++ udagram/udagram-api/src/config/config.ts
@@ -12,7 +12,7 @@ export const config = {
   aws_profile: process.env.AWS_PROFILE,
   aws_media_bucket: process.env.AWS_BUCKET,
   aws_access_key: process.env.AWS_ACCESS_KEY_ID,
-  aws_secret: process.env.AWS_SECRET,
+  aws_secret: process.env.AWS_SECRET_ACCESS_KEY,
   url: process.env.URL,
   jwt: {
     secret: process.env.JWT_SECRET,
